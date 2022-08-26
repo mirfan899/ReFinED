11e767e82e09e24ac47264cd72629a25345e47d0
Muhammad Irfan <virtuoso.irfan@gmail.com>
2022-08-27 01:39:01 +0500

    flask api.

---
+++ src/api.py
@@ -0,0 +1,32 @@
+from flask import Flask, request, jsonify
+from flask_restful import Resource, Api
+from flask_restful import reqparse
+from helpers import *
+
+app = Flask(__name__)
+api = Api(app, prefix="/api/v1")
+
+parser = reqparse.RequestParser()
+parser.add_argument("sentence", type=str, help="sentence or words should be unicode text", location='form')
+
+
+class Refined(Resource):
+    def get(self):
+        return {"message": "Welcome to Refined.", "status": 200}
+
+    def post(self):
+        args = parser.parse_args()
+
+        text = args["sentence"]
+        if text:
+            result = get_refined(text)
+            return jsonify(result)
+        else:
+            return {"sentence parameter is missing": 404}
+
+
+api.add_resource(Refined, "/refined")
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5000, host='0.0.0.0', threading=False, processes=1)
---
+++ src/helpers.py
@@ -0,0 +1,19 @@
+import os
+import time
+from doc_preprocessing.dataclasses import Span
+from refined.processor import Refined
+
+refined = Refined.from_pretrained(model_name='wikipedia_model',
+                                  entity_set='wikipedia',
+                                  data_dir=os.path.join(os.path.expanduser('~'), '.cache/refined/'),
+                                  download_files=True,
+                                  debug=False,
+                                  use_precomputed_descriptions=True,
+                                  requires_redirects_and_disambig=True)
+
+
+def get_refined(text):
+    ents = refined.process_text(text)
+    response = {}
+    for index, ent in enumerate(ents):
+        response[str(index)] = {"text": ent.text, "entity_id": ent.pred_entity_id, "mention_type": ent.coarse_mention_type}
