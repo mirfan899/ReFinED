d6ac2877d7a995c7b09a8dfdca383e765ca6151b
Muhammad Irfan <virtuoso.irfan@gmail.com>
2022-09-09 23:03:30 +0500

    idx added

--- src/helpers.py
+++ src/helpers.py
@@ -15,11 +15,15 @@ refined = Refined.from_pretrained(model_name='wikipedia_model',
 def get_refined(text):
     tokens = word_tokenize(text)
     indexes = []
-    for index, token in enumerate(tokens):
-        indexes.append({"word": token, "index": index})
+    for i, token in enumerate(tokens):
+        indexes.append({"word": token, "index": i})
     ents = refined.process_text(text)
     response = []
     for ent in ents:
+        index = []
+        for o in indexes:
+            if o["word"] in ent.text:
+                index.append(o["index"])
         ent_id = {"wikidata_qcode": ent.pred_entity_id[0]["wikidata_qcode"], "confidence": ent.pred_entity_id[1]}
-        response.append({"text": ent.text, "entity_id": ent_id, "mention_type": ent.coarse_mention_type})
+        response.append({"text": ent.text, "entity_id": ent_id, "mention_type": ent.coarse_mention_type, "idx": index})
     return response
