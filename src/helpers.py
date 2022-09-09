0a0231bf93b2c8698604928dc2df46a1a0e87775
Muhammad Irfan <virtuoso.irfan@gmail.com>
2022-09-09 22:58:06 +0500

    idx added.

--- src/helpers.py
+++ src/helpers.py
@@ -2,7 +2,7 @@ import os
 import time
 from doc_preprocessing.dataclasses import Span
 from refined.processor import Refined
-
+from nltk.tokenize import word_tokenize
 refined = Refined.from_pretrained(model_name='wikipedia_model',
                                   entity_set='wikipedia',
                                   data_dir=os.path.join(os.path.expanduser('~'), '.cache/refined/'),
@@ -13,8 +13,16 @@ refined = Refined.from_pretrained(model_name='wikipedia_model',
 
 
 def get_refined(text):
+    tokens = word_tokenize(text)
+    indexes = []
+    for index, token in enumerate(tokens):
+        indexes.append({"word": token, "index": index})
     ents = refined.process_text(text)
     response = []
     for ent in ents:
-        response.append({"text": ent.text, "entity_id": ent.pred_entity_id, "mention_type": ent.coarse_mention_type})
+        index = []
+        for o in indexes:
+            if o["word"] in ent.text:
+                index.append(o["index"])
+        response.append({"text": ent.text, "entity_id": ent.pred_entity_id, "mention_type": ent.coarse_mention_type, "idx": index})
     return response
